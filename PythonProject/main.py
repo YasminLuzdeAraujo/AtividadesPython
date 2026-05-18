4d1291b5ad91c28a9ba6ab0ecb3848127255d837
YasminLuzdeAraujo <yasminluzdaraujo@gmail.com>
2026-05-17 21:36:46 -0300

    feat: adiciona códigos Python desenvolvidos durante a disciplina

---
+++ PythonProject/main.py
@@ -0,0 +1,9 @@
+def metros_para_centimetros(metros):
+    return metros * 100
+
+
+def celsius_para_fahrenheit(celsius):
+    return (celsius * 9 / 5) + 32
+
+print(metros_para_centimetros(1.5))  # 150 cm
+print(celsius_para_fahrenheit(100))  # 212°F
---
+++ PythonProject/texto.py
@@ -0,0 +1,10 @@
+def contar_letras(texto):
+    return len(texto)
+
+
+def para_maiusculo(texto):
+    return texto.upper()
+
+
+print(contar_letras("Python"))  # 6
+print(para_maiusculo("Python"))  # PYTHON
